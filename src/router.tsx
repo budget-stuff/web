cb239c0108fd28d765d8ab71142f3264b21d2289
a9052681569 <a9052681569@gmail.com>
2023-05-28 17:11:27 +0300

    запрос авторизации перед рутовым роутом

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,11 @@ export const appRouter = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
+		loader: (args): Promise<string> =>
+			fetch('/api/auth').then(r => {
+				console.log(r);
+				return 'hello world!';
+			}),
 		children: []
 	}
 ]);
