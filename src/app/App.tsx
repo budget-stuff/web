d5aab9f271f7fe5b78559e152c1f31c0c9ba0a1d
a9052681569 <a9052681569@gmail.com>
2023-05-28 13:18:43 +0300

    ci test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,7 +19,6 @@ export const App: FC = () => {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 };
