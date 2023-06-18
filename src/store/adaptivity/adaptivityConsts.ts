b492fd5d932d419c3937cfcdc9276dd69ebfce79
a9052681569 <a9052681569@gmail.com>
2023-06-18 21:11:43 +0300

    fix: определение айфона

--- src/store/adaptivity/adaptivityConsts.ts
+++ src/store/adaptivity/adaptivityConsts.ts
@@ -2,9 +2,7 @@ import { DeviceTypes } from './adaptivity';
 
 export const CLIENT_STRINGS: ClientStringData[] = [
 	{ target: 'android', userAgentRegExp: /Android/ },
-	{ target: 'ios', userAgentRegExp: /(iPhone|iPod)/ },
-	{ target: 'mac', userAgentRegExp: /Mac OS X/ },
-	{ target: 'mac', userAgentRegExp: /(Mac OS|MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ }
+	{ target: 'ios', userAgentRegExp: /(iPhone|iPod)/ }
 ];
 
 interface ClientStringData {
