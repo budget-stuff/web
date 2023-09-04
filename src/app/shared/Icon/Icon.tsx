15a0e70582289b28265198f925dfbda422f6ec7c
a9052681569 <a9052681569@gmail.com>
2023-09-04 16:14:33 +0600

    пробую завести иконки в проде

--- src/app/shared/Icon/Icon.tsx
+++ src/app/shared/Icon/Icon.tsx
@@ -3,16 +3,23 @@ import { classNames } from 'src/utils/helpers/classnames';
 
 import './Icon.scss';
 
+const icons = import.meta.glob('../../../assets/icons/*.svg');
+
 export const Icon: FC<IconProps> = ({ name, size = 'medium', ...props }) => {
 	const componentRef = useRef<FC<SVGProps<SVGSVGElement>> | null>(null);
 	const [loadedIconName, setLoadedIconName] = useState<string>('');
 
 	useEffect(() => {
-		import('../../../assets/icons/' + name + '.svg').then(res => {
-			componentRef.current = res.ReactComponent;
-			// обновление стейта помогает триггернуть ре-рендер. Изменение рефа этого не делает
-			setLoadedIconName(name);
-		});
+		// new URL('../../../assets/icons/' + name + '.svg', import.meta.url).href
+		const iconPath = Object.keys(icons).find(path => path.endsWith(name + '.svg'));
+
+		if (iconPath) {
+			icons[iconPath]().then((res: any) => {
+				componentRef.current = res.ReactComponent;
+				// обновление стейта помогает триггернуть ре-рендер. Изменение рефа этого не делает
+				setLoadedIconName(name);
+			});
+		}
 	}, [name]);
 
 	return (
